166569bd058c9490ae5f3acf828a29fe79bd502e
Vladimir Myshkovski <vladimir@eblitz.gg>
2020-12-07 10:43:06 +0400

    Add transaction routes to config/routes/public.js

--- src/config/routes/public.js
+++ src/config/routes/public.js
@@ -3,9 +3,11 @@ const mount = require('koa-mount');
 const accountResource = require('resources/account/public');
 const healthResource = require('resources/health/public');
 const blockResource = require('resources/block/public');
+const transactionResource = require('resources/transaction/public');
 
 module.exports = (app) => {
   app.use(mount('/account', accountResource));
   app.use(mount('/health', healthResource));
   app.use(mount('/block', blockResource));
+  app.use(mount('/transaction', transactionResource));
 };
