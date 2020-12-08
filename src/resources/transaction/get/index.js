562c8171830b9ea3e98b8bd514821ad3b7f0226f
Vladimir Myshkovski <vladimir@eblitz.gg>
2020-12-08 11:29:38 +0400

    Replace local function to imported service's function

--- src/resources/transaction/get/index.js
+++ src/resources/transaction/get/index.js
@@ -1,26 +1,7 @@
-const Joi = require('joi');
-const web3 = require('w3');
-const client = require('cache');
-const validate = require('middlewares/validate');
-
-
-async function getTransactionByNumber(transactionId) {
-  const key = `transaction_hash_${transactionId}`;
-  let value = null;
-  client.get(key, function(err, reply) {
-  	if (reply) {
-  	  value = JSON.parse(reply)
-    }
-  });
-  if (!value) {
-    value = await web3.eth.getTransaction(transactionId);
-    client.setex(key, 60*60, JSON.stringify(value));
-  }
-  return value
-}
+const web3Service = require('services/web3.service');
 
 const handler = async (ctx) => {
-  ctx.body = await getTransactionByNumber(ctx.params.id);
+  ctx.body = await web3Service.getTransactionByNumber(ctx.params.id);
   ctx.status = 200;
 };
 
