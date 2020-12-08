aa6496a40458f70d67280ff59d21588d81058b26
Vladimir Myshkovski <vladimir@eblitz.gg>
2020-12-08 15:20:19 +0400

    Add validator to block resource endpoint

--- src/resources/block/get/index.js
+++ src/resources/block/get/index.js
@@ -1,10 +1,40 @@
 const web3Service = require('services/web3.service');
 
+async function validator(ctx, next) {
+  const number = parseInt(ctx.params.number);
+  if (typeof number !== 'number') {
+    ctx.body = {
+      errors: {
+        number: ['Ethereum number of block must be a number'],
+      },
+    };
+    ctx.throw(400);
+  };
+  if (number <= 0) {
+    ctx.body = {
+      errors: {
+        number: ['Ethereum number of block must be more than 0'],
+      },
+    };
+    ctx.throw(400);
+  };
+  const block = await web3Service.getBlockByNumber(ctx.params.number);
+  if (!block) {
+    ctx.body = {
+      errors: {
+        hash: ['Ethereum number of block is incorrect'],
+      },
+    };
+    ctx.throw(400);
+  }
+  await next();
+}
+
 const handler = async (ctx) => {
-  ctx.body = await web3Service.getBlockByNumber(ctx.params.id);
+  ctx.body = await web3Service.getBlockByNumber(ctx.params.number);
   ctx.status = 200;
 };
 
 module.exports.register = (router) => {
-  router.get('/:id', handler);
+  router.get('/:number', validator, handler);
 };
