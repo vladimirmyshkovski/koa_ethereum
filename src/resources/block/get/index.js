0603aff9159a58b24f49970e7fd0fc56afba05ac
Vladimir Myshkovski <vladimir@eblitz.gg>
2020-12-08 11:29:27 +0400

    Replace local function to imported service's function

--- src/resources/block/get/index.js
+++ src/resources/block/get/index.js
@@ -1,25 +1,7 @@
-const Joi = require('joi');
-const web3 = require('w3');
-const client = require('cache');
-const validate = require('middlewares/validate');
-
-async function getBlockByNumber(numberOfBLock) {
-  const key = `block_number_${numberOfBLock}`;
-  let value = null;
-  client.get(key, function(err, reply) {
-    if (reply) {
-      value = JSON.parse(reply)
-    }
-  });
-  if (!value) {
-    value = await web3.eth.getBlock(numberOfBLock);
-    client.setex(key, 60*60, JSON.stringify(value));
-  }
-  return value
-}
+const web3Service = require('services/web3.service');
 
 const handler = async (ctx) => {
-  ctx.body = await getBlockByNumber(ctx.params.id);
+  ctx.body = await web3Service.getBlockByNumber(ctx.params.id);
   ctx.status = 200;
 };
 
